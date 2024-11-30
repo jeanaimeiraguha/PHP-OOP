302167822e1a9c8ea2f73e77d2ecf590ec6fc607
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-11-30 18:50:54 +0200

    0O0P

--- class/class.php
+++ class/class.php
@@ -6,10 +6,16 @@ class Fruits{
     public $type;
     //Method
     function set_name($name){
-        $this->name= $name;   
+        $this->name= $name; 
+        echo "She loves".$name; 
+     }
+     function  set_type($type){
+        echo "This is in type of".$type;
      }
     
 }
 $Mango =new Fruits();
+$type->set_type("type1");
 $Mango->set_name("Mango");
+
 ?>
